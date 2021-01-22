98af4973ff96b0fdaef0548b37281f7bc15637eb
Şafak Soylu <dawnoble@hotmail.com>
2021-01-22 05:12:48 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ if [ -z "$1" ]; then
 fi
 
 if [ ! -e ngrok.service ]; then
-    git clone --depth=1 https://github.com/vincenthsu/systemd-ngrok.git
+    git clone --depth=1 https://github.com/safaksoylu/homeip.git
     cd systemd-ngrok
 fi
 cp ngrok.service /lib/systemd/system/
